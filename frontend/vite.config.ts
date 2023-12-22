73bd2b58167da0e41e21cd67bd0ee897f293a395
zalaw <keep_razvy@yahoo.com>
2023-12-23 01:48:46 +0200

    Use hardcoded URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    plugins: [react()],
-  });
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
