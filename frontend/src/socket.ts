a38511c42c1eff8e423db8bc95f553ffaab2761f
zalaw <keep_razvy@yahoo.com>
2023-12-27 19:07:47 +0200

    Add .env frontend

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -13,7 +13,7 @@ export const RECONNECTION_ATTEMPTS = 3;
 
 console.log(import.meta.env);
 
-export const socket = io(import.meta.env.PROD ? "https://pixel-mystique.up.railway.app/" : "http://localhost:3001", {
+export const socket = io(import.meta.env.PROD ? import.meta.env.VITE_RAILWAY_STATIC_URL : "http://localhost:3001", {
   autoConnect: false,
   reconnection: true,
   reconnectionDelay: RECONNECTION_DELAY,
